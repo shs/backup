c2b382f0c8e6d15863a9d352efaff8a74c80cc6a
Sebastian von Conrad <sebastian@vonconrad.com>
2012-09-22 14:09:24 +1000

    Set chunk size to 8 MB

--- glacier.rb
+++ glacier.rb
@@ -5,5 +5,5 @@ credentials = YAML.load_file('.aws.yml')
 
 glacier = Fog::AWS::Glacier.new(credentials)
 vault = glacier.vaults.get('shs_backup')
-upload = vault.archives.create description: "#{Date.today} - #{file_name}", body: File.new(file_name), multipart_chunk_size: 4*1024*1024
+upload = vault.archives.create description: "#{Date.today} - #{file_name}", body: File.new(file_name), multipart_chunk_size: 8*1024*1024
 
