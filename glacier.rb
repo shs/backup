5ac46d2355e9f8ee7698f78b54df7726e349f86f
Sebastian von Conrad <sebastian@vonconrad.com>
2012-09-22 14:57:38 +1000

    Make better

--- glacier.rb
+++ glacier.rb
@@ -1,9 +1,12 @@
 require 'fog'
 
 file_name = ARGV.first
+file_description = "#{Date.today} - #{File.basename(file_name)}"
 credentials = YAML.load_file('.aws.yml')
+chunk_size = 8*1024*1024
 
 glacier = Fog::AWS::Glacier.new(credentials)
 vault = glacier.vaults.get('shs_backup')
-upload = vault.archives.create description: "#{Date.today} - #{file_name}", body: File.new(file_name), multipart_chunk_size: 8*1024*1024
+upload = vault.archives.create(description: file_description, body: File.new(file_name), multipart_chunk_size: chunk_size)
 
+puts "#{file_description} uploaded with ID #{upload.id}"
