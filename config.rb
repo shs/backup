f807858b39520a552a0b578ed4c5bf8f8aedec0e
Sebastian von Conrad <sebastian@vonconrad.com>
2012-10-21 12:47:55 +1100

    Use file_backup directory

--- config.rb
+++ config.rb
@@ -15,7 +15,7 @@ Backup::Syncer::Cloud::S3.defaults do |s3|
   s3.secret_access_key = credentials['aws_secret_access_key']
   s3.region            = 'us-east-1'
   s3.bucket            = 'shs-backup'
-  s3.path              = ''
+  s3.path              = 'file_backup'
 end
 
 Dir[File.join(File.dirname(Config.config_file), "models", "*.rb")].each do |model|
