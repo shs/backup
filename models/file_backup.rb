d1b70214259b311738ac694fcbc3bff5f3ccd5f3
Sebastian von Conrad <sebastian@vonconrad.com>
2012-10-21 12:40:35 +1100

    First go of file backup

---
+++ models/file_backup.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+Backup::Model.new(:file_backup, 'Backups the SHS file structure.') do
+  split_into_chunks_of 5000
+
+  sync_with Cloud::S3 do |s3|
+    s3.mirror            = true
+    s3.concurrency_type  = :threads
+    s3.concurrency_level = 50
+
+    s3.directories do |directory|
+      directory.add '/var/www/readme'
+    end
+  end
+end
