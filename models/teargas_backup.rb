65b3cbff86cf7939cd8b1ca63a3565b7cb305350
Sebastian von Conrad <sebastian@vonconrad.com>
2012-10-21 12:57:21 +1100

    Add teargas backup

---
+++ models/teargas_backup.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+Backup::Model.new(:teargas_backup, 'Backups the Teargas backups (meta!).') do
+  split_into_chunks_of 5000
+
+  sync_with Cloud::S3 do |s3|
+    s3.bucket            = 'teargas-backup'
+    s3.path              = 'backups'
+    s3.mirror            = true
+    s3.concurrency_type  = :threads
+    s3.concurrency_level = 50
+
+    s3.directories do |directory|
+      directory.add '/backup/teargas'
+    end
+  end
+end
