ef04ee2436fcc75960d26fae1e167b2259f14879
Sebastian von Conrad <sebastian@vonconrad.com>
2015-08-23 10:36:14 +1000

    Use dotenv (and bundler)

--- config.rb
+++ config.rb
@@ -1,18 +1,19 @@
 # encoding: utf-8
 
-credentials = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), '.aws.yml'))
+require 'dotenv'
+Dotenv.load
 
 Backup::Storage::S3.defaults do |s3|
-  s3.access_key_id     = credentials['aws_access_key_id']
-  s3.secret_access_key = credentials['aws_secret_access_key']
+  s3.access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+  s3.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
   s3.region            = 'us-east-1'
   s3.bucket            = 'shs-backup'
   s3.path              = ''
 end
 
 Backup::Syncer::Cloud::S3.defaults do |s3|
-  s3.access_key_id     = credentials['aws_access_key_id']
-  s3.secret_access_key = credentials['aws_secret_access_key']
+  s3.access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+  s3.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
   s3.region            = 'us-east-1'
   s3.bucket            = 'shs-backup'
   s3.path              = 'file_backup'
