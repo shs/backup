d6dde0341aad0e953feb9a5f3605e15db795519b
Sebastian von Conrad <sebastian@vonconrad.com>
2015-08-23 10:56:48 +1000

    Make settings v4 compatible and turn off mirroring

--- models/file_backup.rb
+++ models/file_backup.rb
@@ -4,9 +4,8 @@ Model.new(:file_backup, 'SHS file structure.') do
   split_into_chunks_of 4000
 
   sync_with Cloud::S3 do |s3|
-    s3.mirror            = true
-    s3.concurrency_type  = :threads
-    s3.concurrency_level = 50
+    s3.mirror       = false
+    s3.threat_count = 50
 
     s3.directories do |directory|
       directory.add '/var/www'
