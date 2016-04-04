d57d63a7543a8f8a0ba381cfa9365367007ae2b1
Sebastian von Conrad <sebastian@vonconrad.com>
2016-04-05 08:13:15 +1000

    Need to step into directory as well

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 every 1.day, :at => '0:00 am' do
-  command "/home/ubuntu/.rbenv/shims/backup perform -t database_backup"
+  command "cd /home/ubuntu/Backup && /home/ubuntu/.rbenv/shims/backup perform -t database_backup"
 end
 
 every 1.week, :at => '0:30 am' do
-  command "/home/ubuntu/.rbenv/shims/backup perform -t file_backup"
+  command "cd /home/ubuntu/Backup && /home/ubuntu/.rbenv/shims/backup perform -t file_backup"
 end
