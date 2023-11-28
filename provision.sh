0d92008c9db9a13d7f0e10e85f810bff5aa85a6a
Sebastian von Conrad <sebastian@vonconrad.com>
2023-11-28 18:09:29 +1100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -186,8 +186,12 @@ sudo a2enmod php5.6
 systemctl restart apache2
 
 
-
-
+# Follow this: https://www.devart.com/dbforge/mysql/how-to-install-mysql-on-ubuntu/
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
+sudo apt update
+sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*
 
 
 
